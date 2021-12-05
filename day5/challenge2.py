8534a47a0697737485f30c6868821575ff084ea3
mut1sya <17155486+mut1sya@users.noreply.github.com>
2021-12-05 20:40:57 +0300

    Add day 5 challenges

---
+++ day5/challenge2.py
@@ -0,0 +1,128 @@
+def read_and_parse_input():
+    with open('input.txt') as f:
+        lines = f.readlines()
+    input_data = []
+    for line in lines:
+        line_coordinates = line.rstrip('\n').split(' -> ')
+        coordinate = {
+            "start": line_coordinates[0].split(','),
+            "end": line_coordinates[1].split(',')
+        }
+        input_data.append(coordinate)
+    return input_data
+
+def setup_matrix(height, width):
+    matrix = []
+    for x in range(height):
+        matrix.append([])
+        for y in range(width):
+            matrix[x].append(0)
+    return matrix
+    
+def determine_if_diagonal(line):
+    x_change = int(line['end'][0])-int(line['start'][0])
+    y_change = int(line['end'][1]) - int(line['start'][1])
+    gradient = float(x_change/y_change)
+    if abs(gradient) == 1:
+        return True
+    else:
+        return False
+
+def determine_type_of_line(line):
+    if line['start'][0] == line['end'][0]:
+        return 'vertical'
+    elif line['start'][1] == line['end'][1]:
+        return 'horizonatal'
+    elif determine_if_diagonal(line):
+        return 'diagonal'
+    else:
+        return "other"
+
+def plot_horizontal_line(line, matrix):
+    line_length = int(line['end'][0]) -  int(line['start'][0]) 
+    if line_length >0:
+        direction = "forward"
+    else:
+        direction = "backward"
+    line_length = abs(line_length)+1 #include the end point
+    
+    update_row = int(line['start'][1])
+    update_forward_point = int(line['start'][0])
+    update_backward_point = int(line['end'][0])
+    for x in range(line_length):
+        if direction == "forward":
+            matrix[update_row][update_forward_point] += 1
+            update_forward_point += 1
+        elif direction == 'backward':
+            matrix[update_row][update_backward_point] += 1
+            update_backward_point +=1
+        else:
+            pass
+
+
+def plot_vertical_line(line, matrix):
+    line_length = int(line['end'][1]) -  int(line['start'][1]) 
+    if line_length >0:
+        direction = "forward"
+    else:
+        direction = "backward"
+    
+    line_length = abs(line_length)+1 # include the end point
+    update_column = int(line['start'][0])
+    update_forward_point = int(line['start'][1])
+    update_backward_point = int(line['end'][1])
+    for x in range(line_length):
+        if direction == "forward":
+            matrix[update_forward_point][update_column] += 1
+            update_forward_point += 1
+        elif direction == 'backward':
+            matrix[update_backward_point][update_column] += 1
+            update_backward_point +=1
+        else:
+            pass
+
+def plot_diagonal_line(line, matrix):
+    x_change = int(line['end'][0])-int(line['start'][0])
+    y_change = int(line['end'][1]) - int(line['start'][1])
+    diagonal_line_length = abs(x_change) +1  # include the end point
+    x_start =int(line['start'][0])
+    y_start = int(line['start'][1])
+    for x in range(diagonal_line_length):
+        matrix[y_start][x_start] += 1
+        if x_change > 0:
+            x_start +=1
+        else:
+            x_start -=1
+        if y_change > 0:
+            y_start +=1
+        else:
+            y_start -=1
+
+
+def compute_score(matrix):
+    sum = 0
+    for row in matrix:
+        for point in row:
+            if point >= 2:
+                sum += 1
+    return sum
+
+
+
+input_data = read_and_parse_input()
+
+matrix = setup_matrix(1000, 1000)
+
+
+for line in input_data:
+    line_type = determine_type_of_line(line)
+    if line_type == "vertical":
+        plot_vertical_line(line, matrix)
+    elif line_type =="horizonatal":
+        plot_horizontal_line(line, matrix)
+    elif line_type == "diagonal":
+        plot_diagonal_line(line, matrix)
+
+score = compute_score(matrix)
+print(score)
+
